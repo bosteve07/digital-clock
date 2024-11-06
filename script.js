8964f209c6656ee0ae17bb997c64cfb0dfaf8e2e
Bolaji <bolajioluwatobi7@gmail.com>
2024-11-06 06:16:28 +0100

    Second Commit

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function updateClock(){
     let hours =now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds(); 
+    let day = now.getDate(); 
+    let month = now.getMonth(); 
+    let year = now.getUTCFullYear();
 
     hours = hours <10 ? "0" + hours : hours;
     minutes = minutes <10 ? "0" + minutes : minutes;
@@ -12,10 +15,17 @@ function updateClock(){
     const hourElement = document.getElementById("hours"); 
     const minutesElement = document.getElementById("minutes");
     const secondsElement = document.getElementById("seconds");
+    const dateElement = document.getElementById("date");
+
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// Get the day of the week as a number (0-6), then use it to find the name
+    const dayName = daysOfWeek[now.getDay()];
 
     hourElement.textContent = hours; 
     minutesElement.textContent = minutes;
     secondsElement.textContent = seconds;
+    dateElement.textContent= `${dayName}, ${day}/${month}/${year}`
 }
 
 setInterval(updateClock, 1000);
